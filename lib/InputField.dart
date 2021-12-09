cc135907293401aa689a958eab243c7b7bdcd692
Alexrp13 <alex.cristian.romera@estudiantat.upc.edu>
2021-12-09 20:40:00 +0100

    first commit

---
+++ lib/InputField.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: Colors.grey[200])
+            )
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Introduce tu correo electrónico",
+              hintStyle: TextStyle(color: Colors.grey),
+              border: InputBorder.none
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: Colors.grey[200])
+              )
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Introduce tu contraseña",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/InputWrapper.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:loginapp/RegisterButton.dart';
+import 'RegisterButton.dart';
+import 'LoginButton.dart';
+import 'InputField.dart';
+
+class InputWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 40,),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: InputField(),
+          ),
+          SizedBox(height: 40,),
+          Text(
+            "¿Has olvidado tu contraseña?",
+            style: TextStyle(color: Colors.grey),
+          ),
+          SizedBox(height: 40,),
+          LoginButton(),
+          SizedBox(height: 20,),
+          RegisterButton(),
+        ],
+      ),
+    );
+  }
+}
